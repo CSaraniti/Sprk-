2b3dbbed37119560b6e98ddddea61a5232e72e59
CSaraniti <CarloSaraniti3@gmail.com>
2019-07-12 14:07:36 -0500

    not much

--- Sprk+/InitialViewController.swift
+++ Sprk+/InitialViewController.swift
@@ -13,8 +13,6 @@ class InitialViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
  
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
