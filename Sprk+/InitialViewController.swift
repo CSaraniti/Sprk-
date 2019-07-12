98ede30ecf04650fe22eff8cd775b0dea7047513
CSaraniti <CarloSaraniti3@gmail.com>
2019-07-12 13:42:36 -0500

    added more stuff

---
+++ Sprk+/InitialViewController.swift
@@ -0,0 +1,23 @@
+//
+//  InitialViewController.swift
+//  Sprk+
+//
+//  Created by Carlo Saraniti on 7/12/19.
+//  Copyright © 2019 Carlo Saraniti. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+    @IBOutlet weak var foodTypeTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+ 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
